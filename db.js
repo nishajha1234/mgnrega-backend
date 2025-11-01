e48b98c4bc113b1869d1b84054e938244acb38bf
Nisha Jha <nishajha1515@gmail.com>
2025-11-01 22:27:41 +0530

    Update database and db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-import path from "path";
-import Database from "better-sqlite3";
-import { fileURLToPath } from "url";
+import Database from 'better-sqlite3';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// ✅ Use absolute path (Render safe)
-const dbPath = path.join(__dirname, "mgnrega.db");
+// Correct absolute path
+const dbPath = path.join(__dirname, 'mgnrega.db');
 
 const db = new Database(dbPath);
 export default db;
